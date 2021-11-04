bca8e87f8397acefa360ba0fe5bdef48b81d1b1e
Francesco <scovascova@gmail.com>
2021-11-04 18:11:15 +0100

    inserimento password

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ console.log('nome')
 
 const cognome = prompt ('Inserisci il tuo cognome')
 console.log('cognome')
+
+const colore = prompt ('Inserisci il tuo colore preferito')
+console.log('colore')
+
+let numero = 21;
+
+let password = nome + cognome + colore + numero;
+document.getElementById('password').innerHTML = password;
+console.log('password')
